0ea328fb46bff07b9dec69533c6d6539225c9456
Gamerbot1212 <gbot03558@gmail.com>
2025-06-20 11:50:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = ""
-api_hash = ""
-bot_token = ""
+api_id = "20831039"
+api_hash = "ea20b722f7af827db12fb85f4d55238c"
+bot_token = "7783327258:AAEmV8oALzVnWYTelh9ZcX2EReu5KMSKbCo"
 auth_users = [7597020624]
